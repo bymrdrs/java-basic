8501ef51d1266a86b8df105b61e659c330aa90db
wangjun <1443746004@qq.com>
2022-09-19 11:17:23 +0800

    Update files in the utils package

--- src/main/java/com/java/basic/utils/JsonUtil.java
+++ src/main/java/com/java/basic/utils/JsonUtil.java
@@ -1,28 +1,47 @@
 package com.java.basic.utils;
 
+import com.java.basic.entity.Person;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+import net.sf.json.JsonConfig;
+
+import java.util.Iterator;
+import java.util.List;
+
 public class JsonUtil {
 
     /**
      * Json转对象List
-     * JSONObject jsonobject = JSONObject.fromObject(responseBody);
-     * JSONObject aggregations = (JSONObject) jsonobject.get("aggregations");
-     * JSONObject groupDate = (JSONObject) aggregations.get("groupDate");
-     * String buckets = groupDate.getString("buckets");
-     * JSONArray bucketsJsonArray = JSONArray.fromObject(buckets);
-     * // 参数1为要转换的JSONArray数据，参数2为要转换的目标数据，即List盛装的数据
-     * List<Buckets> bucketsList = JSONArray.toList(bucketsJsonArray, new Buckets(), new JsonConfig());
      */
+    public static List<Person> jsonToObjectList(String jsonString) {
+        JSONObject jsonobject = JSONObject.fromObject(jsonString);
+        JSONObject aggregations = (JSONObject) jsonobject.get("aggregations");
+        JSONObject groupDate = (JSONObject) aggregations.get("groupDate");
+        String buckets = groupDate.getString("buckets");
+        JSONArray bucketsJsonArray = JSONArray.fromObject(buckets);
+        // 参数1为要转换的JSONArray数据，参数2为要转换的目标数据，即List盛装的数据
+        List<Person> bucketsList = JSONArray.toList(bucketsJsonArray, new Person(), new JsonConfig());
+        return bucketsList;
+    }
 
     /**
      * JSONObject遍历Key
-     * JSONObject rule = JSONObject.fromObject(oaSyncShipment.getRule());
-     * JSONObject countries = rule.getJSONObject("countries");
-     * @SuppressWarnings("unchecked")
-     * Iterator<String> keys = countries.keys();
-     * while (keys.hasNext()) {
-     * String teams = keys.next();// key
-     * String teamsInfo = countries.optString(teams);// value
-     * }
      */
+    @SuppressWarnings("unchecked")
+    public static void jsonForeachKey(String jsonString) {
+        JSONObject jsonobject = JSONObject.fromObject(jsonString);
+        JSONObject aggregations = jsonobject.getJSONObject("aggregations");
+        Iterator<String> keys = aggregations.keys();
+        while (keys.hasNext()) {
+            String teams = keys.next();// key
+            String teamsInfo = aggregations.optString(teams);// value
+            System.out.println(teams + " - " + teamsInfo);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(JsonUtil.jsonToObjectList("{\"aggregations\":{\"groupDate\":{\"buckets\":[{\"name\":\"wangjun\"},{\"name\":\"lisi\"}]}}}"));
+        JsonUtil.jsonForeachKey("{\"aggregations\":{\"groupDate\":{\"buckets\":[{\"name\":\"wangjun\"},{\"name\":\"lisi\"}]}}}");
+    }
 
 }
